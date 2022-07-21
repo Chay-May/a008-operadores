b92a0565160c57895d6e90d80dc5f6ac3eea80d1
Chay-May <chayenne.mayara@gmail.com>
2022-07-20 22:40:38 -0300

    exercícios008-reforço

---
+++ exercicio1/script.js
@@ -0,0 +1,15 @@
+const numpar = Number(prompt('Insira um número par'))
+const restoDiv = numpar % 2
+
+console.log(restoDiv)
+console.log(typeof numpar)
+
+//c) Teste o programa com diversos números pares. Você notou um padrão?
+//Escreva em um comentário de código.
+
+// Resposta: Retorna sempre 'zero'. Todo número par divido por dois não sobra 'resto'
+
+//d) O que acontece no console se o usuário inserir um número ímpar?
+//Escreva em um comentário de código
+
+//Resposta: A divisão funciona, retornando o 'resto' da divisão
